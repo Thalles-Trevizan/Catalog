5c3bae85e91e5dc0a981e28140937c7381ba229a
Thalles Trevizan <thalles_trevizan@hotmail.com>
2026-02-14 04:47:32 -0300

    Add findById

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,5 +20,14 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> categories = repository.findAll();
         return categories.stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> category = repository.findById(id);
+
+        //TODO Add exception handling
+
+        return new CategoryDTO(category.get());
     }
 }
